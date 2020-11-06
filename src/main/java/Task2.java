522aab17d24b15f1d5c87f39a79ecf768dff1a25
Korbinian Reffler <korbinian.reffler@hs-augsburg.de>
2020-11-06 20:28:37 +0100

    adds comments to task2 (b)

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -4,12 +4,12 @@ public class Task2 {
     public static void main(String[] args) throws IOException {
         System.out.println("Please insert a term");
         var term = readLine();
-        validateTerm(term);
+        validateTerm(term); // validates the term
 
-        var op1 = term.charAt(0) == 'T';
-        var op2 = term.charAt(term.length() - 1) == 'T';
+        var op1 = term.charAt(0) == 'T'; // the first character is always the first operand
+        var op2 = term.charAt(term.length() - 1) == 'T'; // the last character is always the operand
 
-        switch (term.substring(1, term.length() - 1)) {
+        switch (term.substring(1, term.length() - 1)) { // crops the operator out of the term
             case "&" -> System.out.printf("%s & %s = %s%n", op1, op2, op1 & op2);
             case "&&" -> System.out.printf("%s && %s = %s%n", op1, op2, op1 && op2);
             case "|" -> System.out.printf("%s | %s = %s%n", op1, op2, op1 | op2);
@@ -21,10 +21,10 @@ public class Task2 {
     private static String readLine() throws IOException {
         String output = "";
         do {
-            output += (char) System.in.read();
-        } while (System.in.available() > 1);
+            output += (char) System.in.read(); // read from the terminal and append to output
+        } while (System.in.available() > 1); // stop as soon as an enter was found
 
-        return output.toUpperCase();
+        return output.toUpperCase(); // converts the term to uppercase so t/f -> T/F
     }
 
     private static void validateTerm(String term) {
@@ -37,24 +37,33 @@ public class Task2 {
     }
 
     protected static void validateTermOrThrow(String term) {
+        // the term has to start (^) with an F or T (T|F)
+        // followed by an operand ((&)|(&&)|(\|)|(\|\|)|(\^))
+        // followed by a F or T again (T|F)
+        // and no aditional characters ($)
         if (term.matches("^(F|T)((&)|(&&)|(\\|)|(\\|\\|)|(\\^))(F|T)$")) {
             // valid
             return;
         }
 
+        // the first letter was neither F nor T
         if (!term.matches("^(F|T).*")) {
             throw new RuntimeException("^\nERROR: should be \"T\" or \"F\"");
         }
 
+        // the second letter was neither & nor | nor ^
         if (!term.matches("^(F|T)((&)|(\\|)|(\\^)).*")) {
             throw new RuntimeException(" ^\nERROR: should be \"&\", \"|\" or \"^\"");
         }
 
+        // the third letter was neither an & nor | if (started with & or |)
+        // and the third letter was no F or T
         if (!term.matches("^(F|T)((&)|(\\|)|(\\^))|(F|T).*")
                 || !term.matches("^(F|T)((&&)|(\\|\\|)).*")) {
             throw new RuntimeException("  ^\nERROR: should be \"T\", \"F\", \"&\", \"|\"");
         }
 
+        // the term has || or && as operand but it was not followed by F or T
         if (!term.matches("^(F|T)((&)|(&&)|(\\|)|(\\|\\|)|(\\^))(F|T).*")) {
             throw new RuntimeException("  ^\nERROR: should be \"T\" or \"F\"");
         }
